46edadb5f1c7a33f0924e63a1539d5f9e407d9a4
caiomfortes <caio.mfortes@gmail.com>
2024-10-28 16:03:47 -0300

    Adição de cidades + modal de confirmação + alert

--- financeiro_angelim/contabilidade/views.py
+++ financeiro_angelim/contabilidade/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import *
+from django.views.decorators.csrf import csrf_protect
+
 
 # Create your views here.
 
@@ -26,21 +28,23 @@ def contas_content(request):
     return render(request,'contas_content.html', {})
 
 
-
 def cidades(request):
     if(request.method == 'POST'):
         nome = request.POST.get('nome')
+        nome = nome.strip()
         ativo = request.POST.get('status')
         if(Cidade.objects.filter(nome__iexact=nome).exists()):
             data = {
-                'msg': "Esse nome já está em uso."
+                'msg': "existente"
             }
             return JsonResponse(data)
         
         else:
             consulta = Cidade(nome=nome, ativo=ativo)
+            print(nome, ativo)
+            consulta.save()
             data = {
-                'msg':"Adicionado!"
+                'msg':"adicionado"
             }
             return JsonResponse(data)
 
