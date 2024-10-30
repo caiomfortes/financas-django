e8c97af962a85e655de6a770c2e3579ff965441b
caiomfortes <caio.mfortes@gmail.com>
2024-10-30 02:14:19 -0300

    Database

--- financeiro_angelim/financeiro_angelim/settings.py
+++ financeiro_angelim/financeiro_angelim/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'financeiro_angelim.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        "NAME": "angelim_count",
-        "USER": "root",
-        "PASSWORD": "root",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
-    }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         "NAME": "angelim_count",
+#         "USER": "root",
+#         "PASSWORD": "root",
+#         "HOST": "127.0.0.1",
+#         "PORT": "3306",
+#     }
+# }
 
 
 # Password validation
